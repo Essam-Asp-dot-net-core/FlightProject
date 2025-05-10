50422d0bd41df500847a9b261b9ea858133ed181
Essam102 <ahmedessam.1593500@gmail.com>
2025-05-10 23:04:21 +0300

    Add project files.

---
+++ Fight.Core/Entities/Airplane.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fight.Core.Entities
+{
+	public class Airplane : BaseEntity
+	{
+		public string Model { get; set; }
+		public string Manufacturer { get; set; }
+		public int Capacity { get; set; }
+		public string RegistrationNumber { get; set; }
+
+		public ICollection<Flight> Flights { get; set; }
+
+	}
+}
